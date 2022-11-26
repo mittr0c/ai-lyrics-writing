f84a3ceefd5fa2fd48d786ca001751886988c746
mittr0c <118056789+mittr0c@users.noreply.github.com>
2022-11-26 15:50:17 +0800

    Update 一键整理歌词.py

--- data/一键整理歌词.py
+++ data/一键整理歌词.py
@@ -1,10 +1,10 @@
-import os,json
+import os
 import re
 
 
 file = os.listdir('歌曲')
 
-list = []
+slist = []
 
 for songs in file:
     with open(f'歌曲/{songs}', 'r+', encoding='utf-8') as f:
@@ -14,15 +14,23 @@ for songs in file:
         songs1 = re.sub('\r', '/', songs1)
         songs1 = re.sub('///', '/', songs1)
         songs1 = re.sub('//', '/', songs1)
+        songs1 = re.sub('  ', ' ', songs1)
+        song0 = songs1.split('/')
+        song0 = list(set(song0))
+        song2 = str()
+        for sent in song0:
+            song2 += sent + '/'
+
         f.seek(0)  # 指向文本开头
         f.truncate()  # 清空文本
-        f.write(songs1)
-    list.append(songs1)
+        f.write(song2)
+
+    slist.append(songs1)
     print('已处理：',songs)
 
 lyrics = str()
 
-for songs1 in list:
+for songs1 in slist:
     lyrics = lyrics + songs1 + '\n'
 
 with open(f'已完成/lyrics.txt', 'w+', encoding='utf-8') as f:
